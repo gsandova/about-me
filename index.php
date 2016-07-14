82444c72068fac311efea6dbb68852d25d967f2d
Gerald Sandoval <gsandoval16@cnm.edu>
2016-07-13 20:04:26 -0600

    2nd commit

--- index.php
+++ index.php
@@ -5,24 +5,61 @@
 
 
 
-		<title>:) My Basic HTML Page :) </title>
+		<title>Full of myself </title>
 	</head>
 	<body>
 		<header>
-			<h1>Hello, World! :D</h1>
+			<h1>My Me Me Me MEEEEEE Page</h1>
 		</header>
 
-		<h3>** Make it so **</h3>
+		<h3>** Who am I? **</h3>
+		<h2>** Who am I? **</h2>
+		<h1>** Who am I? **</h1>
+		<h4>** Who am I? **</h4>
 		<!--.Start.Main.Content.Here.-->
 		<!---->
 		<!---->
 
 		<main>
 			<!--.Comment.Text.Goes.Here.-->
+			<img src="image/Ed.jpg" alt="Me" />
 
 			<p>
 				Unidentified <strong> nuclear wessels</strong> travelling at sub warp speed, bearing 235.73. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.
 			</p>
+
+			<h1>Education:</h1>
+
+			<p>
+				Football Scholarship at Wossamotta U
+			</p>
+			<img src="image/wossamotta.jpg" alt="Me" />
+
+			<h1>Profession:</h1>
+
+			<p>
+				Crash test dummy at General Motors.
+			</p>
+			<img src="image/images.jpg" alt="Me" />
+
+			<h1>Turn Offs:</h1>
+
+			<h3>Facebook</h3>
+			<h4>Facebook Zucks!</h4>
+			<img src="image/love-china-love-tank.jpg" alt="Me" />
+
+			<h1>Hobbies:</h1>
+			<p>
+				Sampling at Ben & Jerry's
+			</p>
+			<img src="image/Ben-Jerrys.jpg" alt="Me" />
+
+			<p>
+				Sampling at Tillamook Cheese Factor
+			</p>
+			<img src="image/tillamook.png" alt="Me" />
+
+
 			<ul>
 				<li>item 1</li>
 				<li>item 2</li>
@@ -38,7 +75,7 @@
 			<div>
 				<a href="https://cracked.com" target="_blank">go to cracked.com</a>
 			</div>
-			<img src="images/547462_v1.jpg" alt="times square" />
+			
 			<table>
 				<tr>
 					<td>
